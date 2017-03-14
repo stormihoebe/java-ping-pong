6670b13fed793a25401a9b6d4dfc90bc02edb71e
Kaya Winn and Stormi Hoebelheinrich <kaya@>
2017-03-14 16:21:50 -0700

    create pingpong app

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PingPong {
+
+  public List<Object> runPingPong(int countUpTo){
+    List<Object> result = new ArrayList<Object>();
+    for (int i = 1; i <= countUpTo; i++){
+      if(i % 15 == 0){
+        result.add("pingpong");
+      } else if(i % 5 == 0){
+        result.add("pong");
+      } else if(i % 3 == 0){
+        result.add("ping");
+      } else  {
+      result.add(i);
+      }
+    }
+    return result;
+  }
+
+}
